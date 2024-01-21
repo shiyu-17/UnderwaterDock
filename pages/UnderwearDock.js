2d945b614fc7c7a06dc8e7ed9bbf14d6be1dd576
Leo <shiyu_l7@163.com>
2024-01-21 14:57:21 +0800

    initial design

--- pages/UnderwearDock.js
+++ pages/UnderwearDock.js
@@ -1,12 +1,33 @@
 // pages/UnderwearDock.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+/**
+     * 页面的初始数据
+     */
+    data: {
+      result:'等待获取token',
   },
+  /**
+   * 获取token按钮按下：
+   */
+  touchBtn_gettoken:function()
+  {
+      console.log("获取token按钮按下");
+  },
+  /**
+   * 获取设备影子按钮按下：
+   */
+  touchBtn_getshadow:function()
+  {
+      console.log("获取设备影子按钮按下");
+  },
+   /**
+   * 设备命令下发按钮按下：
+   */
+  touchBtn_setCommand:function()
+  {
+      console.log("设备命令下发按钮按下");
+  },  
 
   /**
    * 生命周期函数--监听页面加载
